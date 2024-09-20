f504a8aa068f3e5e27e9aa4ff9ad746461fa2089
“kilo-come” <1544791933@qq.com>
2024-09-20 19:55:34 +0800

    能行吗

---
+++ 小飞机.c
@@ -0,0 +1,25 @@
+# include<stdio.h>
+int Add(int a, int b)
+{
+	return a*b;
+}
+int main ()
+{
+	printf ("     **\n");
+	printf("     **\n");
+	printf("************\n") ;
+	printf("    *  *\n");
+	
+	//int a = 2;
+	//int b = 8;
+	//int c = Add(a, b);
+	//printf("%d", c);
+
+	
+	return 0;
+
+}
+//在线oj,非常流行
+// 所有代码都是自己去输入输出io型
+//接口型
+//只需要函数，oj型
